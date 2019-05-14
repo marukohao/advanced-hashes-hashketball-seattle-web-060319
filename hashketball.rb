0684daddc761f86c574103905cd3c8930ef5e7ae
hao wang <ge_nah@hotmail.com>
2019-05-14 19:10:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -72,7 +72,7 @@ def game_hash
           :blocks => 7,
           :slam_dunks => 2         
         },
-        "Bismck Biyombo" => {
+        "Bismack Biyombo" => {
           :number => 0,
           :shoe => 16,
           :points => 12,
