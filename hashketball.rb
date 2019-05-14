c0d53fbe57f6908a9f3a3db5a86954b63dda64a4
hao wang <ge_nah@hotmail.com>
2019-05-14 18:07:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -82,7 +82,7 @@ def game_hash
           :blocks => 15,
           :slam_dunks => 10         
         },
-        "Besagno Diop" => {
+        "DeSagna Diop" => {
           :number => 2,
           :shoe => 14,
           :points => 24,
