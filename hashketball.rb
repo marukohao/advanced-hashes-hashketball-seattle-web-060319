3fd0936d5a016bef506c2571235e5b48a3c135d4
hao wang <ge_nah@hotmail.com>
2019-05-14 17:32:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,143 @@
 # Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"], 
+      :players => {
+        :alan_anderson => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        :reggie_evans => {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7          
+        },
+        :brook_lopez => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15          
+        },
+        :moson_plumlee => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5         
+        },
+        :jason_terry => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1          
+        }
+      }
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"], 
+      :players => {
+        :jeff_adrien => {
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2         
+        },
+        :bismack_biyombo => {
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10         
+        },
+        :desagno_diop => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5           
+        },
+        :ben_gordon => {
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0          
+        },
+        :brendan_haywood => {
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12          
+        }
+      }      
+    }
+  }
+end
 
 
-
-
-
-
+def num_points_scored(name)
+  count = 0 
+  array = name.split(" ")
+  new_name = array.join("_").downcase
+  game_hash.each do |location, teamdata| 
+    teamdata.each do |key, value|
+      if key == :players
+        value.each do |names, item|
+          if names == ":" + new_name
+            item.each do |items, score|
+              if items == :points
+                count = score  
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  count
+end
 
 
 
